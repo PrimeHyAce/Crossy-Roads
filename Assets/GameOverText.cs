9ea70925b96b1a6a8aa4475cb6fa81c202b916fb
Faizeennn <muhammad1802ft.2020@student.uny.ac.id>
2023-05-08 13:22:46 +0700

    add highscore

--- Assets/GameOverText.cs
+++ Assets/GameOverText.cs
@@ -6,9 +6,35 @@ using UnityEngine;
 public class GameOverText : MonoBehaviour
 {
     [SerializeField] TMP_Text gameOverText;
+    [SerializeField] TMP_Text highScoreText;
+
+    int previousHighScore;
 
     public void UpdateText(int score)
     {
         gameOverText.text = $"Game Over\nScore: {score}";
     }
+
+    private void Start() {
+        if (PlayerPrefs.HasKey("HighScore")){
+            previousHighScore = PlayerPrefs.GetInt("HighScore", 0);
+            highScoreText.text = "Highest: " + previousHighScore.ToString();
+        }
+    }
+
+    public void UpdateHighest(int score)
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            if (PlayerPrefs.GetInt("HighScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+    
 }
--- Assets/ScoreText.cs
+++ Assets/ScoreText.cs
@@ -6,9 +6,34 @@ using UnityEngine;
 public class ScoreText : MonoBehaviour
 {
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text highScoreText;
+
+    int score;
 
     public void UpdateScore(int score)
     {
+        this.score = score;
         scoreText.text = score.ToString();
+    }
+
+    private void Start() {
+        if (PlayerPrefs.HasKey("HighScore")){
+            highScoreText.text = "Highest: " + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        }
+    }
+
+    public void UpdateHighest()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            if (PlayerPrefs.GetInt("HighScore") < this.score)
+            {
+                PlayerPrefs.SetInt("HighScore", this.score);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HighScore", this.score);
+        }
     }
 }
